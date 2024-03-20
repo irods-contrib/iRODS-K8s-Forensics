6ebc2b0248d8d826e1cc082298c8018837638da9
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-03-20 17:37:34 -0400

    cleaning up enum names

--- src/common/enum_utils.py
+++ src/common/enum_utils.py
@@ -16,11 +16,18 @@ class ReturnCodes(int, Enum):
     Class enum for error codes
     """
     EXIT_CODE_SUCCESS = 0
-    TEST_FOUND_FAILURE = 0
-    TEST_FOUND_SUCCESS = 1
+
+    TEST_RESULTS_NOT_FOUND = 0
+    TEST_RESULTS_FOUND = 1
+
     DB_ERROR = -1
+
     EXCEPTION_RUN_PROCESSING = -99
+
+    RESULT_PARSE_SUCCESS = 0
+
     ERROR_TIMEOUT = -98
     ERROR_NO_RUN_DIR = -97
     ERROR_NO_RUN_DATA = -96
     ERROR_NO_TESTS = -95
+    ERROR_RESULT_PARSE_FAILURE = -94
