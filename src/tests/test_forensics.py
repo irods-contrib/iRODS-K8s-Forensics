6a57e7b2a946bfc858384537b836858074284100
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-03-22 08:23:41 -0400

    updating/ adding test for the xml parsing

--- src/tests/test_forensics.py
+++ src/tests/test_forensics.py
@@ -14,7 +14,7 @@ from src.forensics.forensics import Forensics
 from src.common.enum_utils import ReturnCodes
 
 
-#@pytest.mark.skip(reason="Local test only")
+@pytest.mark.skip(reason="Local test only")
 def test_run():
     """
     tests doing the normal operations for initial and final forensics.
@@ -42,7 +42,7 @@ def test_run():
     assert ret_val == ReturnCodes.ERROR_NO_RUN_DIR
 
     # set a run ID
-    run_id: str = '68'
+    run_id: str = '67'
 
     # set up the test directory
     run_dir: str = os.path.join(os.getenv('TEST_PATH'))
@@ -58,6 +58,9 @@ def test_run():
 def test_parse_test_results():
     """
     tests the parsing of a test results xml file
+
+    this test requires that DB connection parameters are set
+
     :return:
     """
     # init the return value
@@ -69,11 +72,32 @@ def test_parse_test_results():
     # set a run ID
     run_id: str = '67'
 
-    # set up the test directory
-    run_dir: str = os.path.join(os.getenv('TEST_PATH'), 'PROVIDER')
+    # init the run directory. there is no CONSUMER data directory for this test
+    run_dir: str = os.path.join(os.getenv('TEST_PATH'), run_id, 'CONSUMER')
+
+    # make the call to do parse the test results
+    ret_val = forensics.parse_test_reports(run_id, run_dir)
+
+    # make sure of a successful return code and a .complete file
+    assert ret_val == ReturnCodes.ERROR_NO_RESULT_DIR
+
+    # set up the test directory. there is a PROVIDER data directory for this test
+    run_dir: str = os.path.join(os.getenv('TEST_PATH'), run_id, 'PROVIDER')
 
     # make the call to do parse the test results
     ret_val = forensics.parse_test_reports(run_id, run_dir)
 
     # make sure of a successful return code and a .complete file
     assert ret_val == ReturnCodes.EXIT_CODE_SUCCESS
+
+    # set a run ID
+    run_id: str = '68'
+
+    # init the run directory. there is a PROVIDER data directory but no test files for this one
+    run_dir: str = os.path.join(os.getenv('TEST_PATH'), run_id, 'PROVIDER')
+
+    # make the call to do parse the test results
+    ret_val = forensics.parse_test_reports(run_id, run_dir)
+
+    # make sure of a successful return code and a .complete file
+    assert ret_val == ReturnCodes.ERROR_NO_RESULT_DATA
